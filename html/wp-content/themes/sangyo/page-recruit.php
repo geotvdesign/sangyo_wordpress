52d4f7eba9a2aa0558b0a52c66d7224b791ef59b
hosodatakahiro <634wave@gmail.com>
2020-09-28 17:29:59 +0900

    採用情報ページ概要制作

---
+++ html/wp-content/themes/sangyo/page-recruit.php
@@ -0,0 +1,131 @@
+<?php get_header();?>
+<section class="under-contents under-recruit">
+  <div class="under-contents__inner">
+    <h1 class="under-contents__ttl">
+      <div class="under-contents__ttl__en">Recruit</div>
+      <div class="under-contents__ttl__ja">採用情報</div>
+    </h1>
+  </div>
+</section>
+<section class="human">
+  <div class="human__inner">
+    <h2 class="human__ttl">
+      <div class="human__ttl__en">Human</div>
+      <div class="human__ttl__jp">求める人材</div>
+    </h2>
+    <div class="human__detail">
+      <div class="human__detail__inner">
+        <div class="human__detail__box">
+          <div class="human__detail__box__inner">
+            <div class="colum">
+              <div class="human__detail__box__contents col-md-6">
+                <div class="human__detail__box__contents__inner">
+                  <p class="human__detail__box__contents__ttl">海外工場での製作により、国内製作よりコストカットができます</p>
+                  <p class="human__detail__box__contents__info">商社を介さず、当社の現地工場にて製作いたしますので、マージンや製作コストが削減できます。</p><a href="http://">
+                    <div class="btn btn-outline-dark btn-block btn-lg">詳しくはこちら</div></a>
+                </div>
+              </div>
+              <div class="human__detail__box__img col-md-6"><img src="https://placehold.jp/554x436.png" alt=""/></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="interview">
+  <div class="interview__inner">
+    <h2 class="interview__ttl">
+      <div class="interview__ttl__en">Interview</div>
+      <div class="interview__ttl__jp">インタビュー</div>
+    </h2>
+    <div class="row">
+      <div class="top-recruit__desc__box col-sm">
+        <div class="top-recruit__desc__box__figure"><a href="http://"><img src="https://placehold.jp/500x600.png" alt=""/></a>
+          <h3 class="top-recruit__desc__box__figure__copy"></h3>
+        </div>
+      </div>
+      <div class="top-recruit__desc__box col-sm">
+        <div class="top-recruit__desc__box__figure"><a href="http://"><img src="https://placehold.jp/500x600.png" alt=""/></a>
+          <h3 class="top-recruit__desc__box__figure__copy"></h3>
+        </div>
+      </div>
+      <div class="top-recruit__desc__box col-sm">
+        <div class="top-recruit__desc__box__figure"><a href="http://"><img src="https://placehold.jp/500x600.png" alt=""/></a>
+          <h3 class="top-recruit__desc__box__figure__copy"></h3>
+        </div>
+      </div>
+      <div class="top-recruit__desc__box col-sm">
+        <div class="top-recruit__desc__box__figure"><a href="http://"><img src="https://placehold.jp/500x600.png" alt=""/></a>
+          <h3 class="top-recruit__desc__box__figure__copy"></h3>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="application">
+  <div class="application__inner">
+    <h2 class="application__ttl">
+      <div class="application__ttl__en">Application</div>
+      <div class="application__ttl__jp">募集要項</div>
+    </h2>
+    <dl>
+      <dt> 会社名</dt>
+      <dd> ASTRO BLADE株式会社（英字登録 読み＝アストロブレード</dd>
+    </dl>
+    <dl>
+      <dt> 設　立</dt>
+      <dd> 平成19年5月</dd>
+    </dl>
+    <dl>
+      <dt> 代表者</dt>
+      <dd> 代表取締役　見崎 操</dd>
+    </dl>
+    <dl>
+      <dt> 本　店</dt>
+      <dd> 千葉県千葉市花見川区検見川町1-591-2<br/>TEL：<span class="tel-num-white">043-441-7008</span>　<br class="sp-only">FAX：<span class="tel-num-white">0434-41-7015</span></dd>
+    </dl>
+    <dl>
+      <dt> 研究所</dt>
+      <dd> 埼玉県草加市青柳1-5</dd>
+    </dl>
+    <dl>
+      <dt> 工　場</dt>
+      <dd> 中国遼寧省大連市甘井子区毛墓子村北海工業地区</dd>
+    </dl>
+    <dl>
+      <dt>中国大連事務所</dt>
+      <dd>大連市甘井子区&#28377;仁南園7-2-101</dd>
+    </dl>
+    <dl>
+      <dt>中国大連ショールーム</dt>
+      <dd>大連市甘井子区毛墓子北海工業区</dd>
+    </dl>
+    <dl>
+      <dt>資本金</dt>
+      <dd>5,000万円</dd>
+    </dl>
+    <dl>
+      <dt>従業員</dt>
+      <dd>12名</dd>
+    </dl>
+    <dl>
+      <dt>グループ企業</dt>
+      <dd>ニューサンライト株式会社（日本）<br/>日本クリーンアップ株式会社（日本）<br/>台湾五常有限公司（中華民国 台北市）<br/>大連ASTRO BLADE有限公司（中国 遼寧省）<br/>ASTRO BLADE VN.CO（ベトナム・HMC）</dd>
+    </dl>
+    <dl>
+      <dt>支　所</dt>
+      <dd>中華人民共和国（遼寧省、大連市）<br/>タイ王国（バンコク市、スクンビット区）</dd>
+    </dl>
+  </div>
+</section>
+<section class="under-contact">
+  <div class="under-contact__inner">
+    <h2 class="under-contact__ttl">
+      <div class="under-contact__ttl__en">Contact</div>
+      <div class="under-contact__ttl__jp">お問い合わせ</div>
+    </h2>
+    <p class="under-contact__detail">ご意見・ご質問など、お問い合わせは、下記フォームからお願いいたします。</p>
+    <div class="under-contact__btn"><a href="http://">Contact Form</a></div>
+  </div>
+</section><?php get_footer();?>
