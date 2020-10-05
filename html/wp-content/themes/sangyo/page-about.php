9ec8c1fb82f2ee2928181630c9f38a2cde104fc3
geotvdesign <45956520+geotvdesign@users.noreply.github.com>
2020-10-05 16:42:04 +0900

    当社の強みページ文言修正

--- html/wp-content/themes/sangyo/page-about.php
+++ html/wp-content/themes/sangyo/page-about.php
@@ -14,12 +14,12 @@
         <div class="colum">
           <div class="about__box__contents col-md-6">
             <div class="about__box__contents__inner">
-              <p class="about__box__contents__ttl">海外工場での製作により、国内製作よりコストカットができます</p>
-              <p class="about__box__contents__info">商社を介さず、当社の現地工場にて製作いたしますので、マージンや製作コストが削減できます。</p><a href="http://">
+              <p class="about__box__contents__ttl">MOLグループネットワークを生かした付加価値の提供</p>
+              <p class="about__box__contents__info">当社は世界最大規模の海運会社である商船三井（MOL）のグループ企業として、海上・陸上のあらゆるニーズに技術力と提案力でお応えする技術商社です。永年培われてきたノウハウと、グループネットワーク、さらに「チャレンジ」の精神により、お客様のニーズにお応えし、さらなる付加価値の提供をお約束します。</p><a href="http://">
                 <div class="btn btn-outline-dark btn-block btn-lg">詳しくはこちら</div></a>
             </div>
           </div>
-          <div class="about__box__img col-md-6"><img src="https://placehold.jp/554x436.png" alt=""/></div>
+          <div class="about__box__img01 col-md-6"></div>
         </div>
       </div>
     </div>
@@ -28,12 +28,12 @@
         <div class="colum">
           <div class="about__box__contents col-md-6">
             <div class="about__box__contents__inner">
-              <p class="about__box__contents__ttl">海外工場での製作により、国内製作よりコストカットができます</p>
-              <p class="about__box__contents__info">商社を介さず、当社の現地工場にて製作いたしますので、マージンや製作コストが削減できます。</p><a href="http://">
+              <p class="about__box__contents__ttl">海上・陸上のあらゆるニーズに対応</p>
+              <p class="about__box__contents__info">商船三井グループの中核商事会社として、船舶用の燃料油・潤滑油の販売や各種機器、資材、部品、通信関連機材の販売は勿論のこと、陸上用の空調機器、各種プラントを対象とした産業用機器の販売や、中古コンテナの販売・レンタルなど取扱商品は多種にわたります。これら商品に私どもの付加価値を添えて、皆様のニーズにお応えして参ります。</p><a href="http://">
                 <div class="btn btn-outline-dark btn-block btn-lg">詳しくはこちら</div></a>
             </div>
           </div>
-          <div class="about__box__img col-md-6"><img src="https://placehold.jp/554x436.png" alt=""/></div>
+          <div class="about__box__img02 col-md-6"></div>
         </div>
       </div>
     </div>
@@ -42,12 +42,12 @@
         <div class="colum">
           <div class="about__box__contents col-md-6">
             <div class="about__box__contents__inner">
-              <p class="about__box__contents__ttl">海外工場での製作により、国内製作よりコストカットができます</p>
-              <p class="about__box__contents__info">商社を介さず、当社の現地工場にて製作いたしますので、マージンや製作コストが削減できます。</p><a href="http://">
+              <p class="about__box__contents__ttl">グループ一丸となった環境・安全への取り組み</p>
+              <p class="about__box__contents__info">商船三井グループでは「環境先進企業」として、環境負荷低減に向けた取り組みを一層強化しており、商船三井テクノトレードでは環境保全方針を定め、エコアクション21を認証取得しています。詳しくは環境保全方針の欄をご覧ください。更に、安全に向けた取り組みでは、商船三井の「世界最高水準の安全運航」の実現をサポートし、陸上と海上が一体となって、4ゼロをはじめとする目標達成に取り組んでいます。</p><a href="http://">
                 <div class="btn btn-outline-dark btn-block btn-lg">詳しくはこちら</div></a>
             </div>
           </div>
-          <div class="about__box__img col-md-6"><img src="https://placehold.jp/554x436.png" alt=""/></div>
+          <div class="about__box__img03 col-md-6"></div>
         </div>
       </div>
     </div>
