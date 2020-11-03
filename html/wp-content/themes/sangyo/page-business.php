2186f2297eb3f7421e5c93b832021ad5a3f59409
geotvdesign <45956520+geotvdesign@users.noreply.github.com>
2020-11-03 22:14:46 +0900

    リンクホバーアクション追加

--- html/wp-content/themes/sangyo/page-business.php
+++ html/wp-content/themes/sangyo/page-business.php
@@ -4,42 +4,41 @@
     <h1 class="under-contents__ttl">
       <div class="under-contents__ttl__en">Business</div>
       <div class="under-contents__ttl__ja">事業内容</div>
+      <section class="top-business"></section>
     </h1>
   </div>
 </section>
-<section class="business">
-  <div class="business__inner">
-    <div class="colum fadein">
-      <div class="business__box col-md-6"><a href="./metal/">
-          <div class="business__box__bnr top-metal">
-            <div class="business__box__bnr__inner">
-              <div class="business__box__bnr__name fadein">金属・鉄鋼部門</div>
-              <p class="business__box__bnr__info fadein">社会インフラを支える鉄と金属</p>
-            </div>
-          </div></a></div>
-      <div class="business__box col-md-6"><a href="./food/">
-          <div class="business__box__bnr top-food">
-            <div class="business__box__bnr__inner">
-              <div class="business__box__bnr__name fadein">食料部門</div>
-              <p class="business__box__bnr__info fadein">暮らしを支える食料</p>
-            </div>
-          </div></a></div>
-    </div>
-    <div class="colum fadein">
-      <div class="business__box col-md-6"><a href="./machine/">
-          <div class="business__box__bnr top-machine">
-            <div class="business__box__bnr__inner">
-              <div class="business__box__bnr__name fadein">機械部門</div>
-              <p class="business__box__bnr__info fadein">自動車から専門機器まで</p>
-            </div>
-          </div></a></div>
-      <div class="business__box col-md-6"><a href="./energy/">
-          <div class="business__box__bnr top-energy">
-            <div class="business__box__bnr__inner">
-              <div class="business__box__bnr__name fadein">エネルギー・化学品部門</div>
-              <p class="business__box__bnr__info fadein">日本を支えるエネルギー</p>
-            </div>
-          </div></a></div>
-    </div>
+<section class="top-business__inner page">
+  <div class="colum fadein">
+    <div class="top-business__box col-md-6"><a href="./metal/">
+        <div class="top-business__box__bnr top-metal">
+          <div class="top-business__box__bnr__inner">
+            <div class="top-business__box__bnr__name fadein">金属・鉄鋼部門</div>
+            <p class="top-business__box__bnr__info fadein">社会インフラを支える鉄と金属</p>
+          </div>
+        </div></a></div>
+    <div class="top-business__box col-md-6"><a href="./food/">
+        <div class="top-business__box__bnr top-food">
+          <div class="top-business__box__bnr__inner">
+            <div class="top-business__box__bnr__name fadein">食料部門</div>
+            <p class="top-business__box__bnr__info fadein">暮らしを支える食料</p>
+          </div>
+        </div></a></div>
+  </div>
+  <div class="colum fadein">
+    <div class="top-business__box col-md-6"><a href="./machine/">
+        <div class="top-business__box__bnr top-machine">
+          <div class="top-business__box__bnr__inner">
+            <div class="top-business__box__bnr__name fadein">機械部門</div>
+            <p class="top-business__box__bnr__info fadein">自動車から専門機器まで</p>
+          </div>
+        </div></a></div>
+    <div class="top-business__box col-md-6"><a href="./energy/">
+        <div class="top-business__box__bnr top-energy">
+          <div class="top-business__box__bnr__inner">
+            <div class="top-business__box__bnr__name fadein">エネルギー・化学品部門</div>
+            <p class="top-business__box__bnr__info fadein">日本を支えるエネルギー</p>
+          </div>
+        </div></a></div>
   </div>
 </section><?php get_footer();?>
